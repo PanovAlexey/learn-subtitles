d113cbeae76c8bb1e1be24c03892b29e194c9988
Panov Alexey <alexeykapanov@gmail.com>
2022-08-27 18:19:51 +0300

    Add service name parameter to config and env file.

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,32 +8,39 @@ import (
 var configSingleton *Config
 
 type Config struct {
+	applicationName  string
 	telegramBotToken string
 }
 
 func NewConfig() (*Config, error) {
 	if configSingleton == nil {
 		config := &Config{}
-		err := initConfigByEnv(config)
+		err := config.initConfigByEnv()
 
 		if err != nil {
 			return nil, err
 		}
 
+		config.initConfigByDefault()
 		configSingleton = config
 	}
 
 	return configSingleton, nil
 }
 
+func (c Config) GetApplicationName() string {
+	return c.applicationName
+}
+
 func (c Config) GetTelegramBotToken() string {
 	return c.telegramBotToken
 }
 
-func initConfigByEnv(config *Config) error {
-	config.telegramBotToken = getEnv("TELEGRAM_BOT_TOKEN")
+func (c *Config) initConfigByEnv() error {
+	c.applicationName = getEnv("APPLICATION_NAME")
+	c.telegramBotToken = getEnv("TELEGRAM_BOT_TOKEN")
 
-	if len(config.telegramBotToken) < 1 {
+	if len(c.telegramBotToken) < 1 {
 		return errors.New("telegram bot token is empty")
 	}
 
@@ -46,4 +53,10 @@ func getEnv(key string) string {
 	}
 
 	return ""
+}
+
+func (c *Config) initConfigByDefault() {
+	if len(c.applicationName) < 1 {
+		c.applicationName = "Learn subtitles"
+	}
 }

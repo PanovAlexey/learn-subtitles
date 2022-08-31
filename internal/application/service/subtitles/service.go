4c08a98def48c8509f7321fc27dda779b0391e3f
Panov Alexey <alexeykapanov@gmail.com>
2022-08-31 17:23:17 +0300

    Create blanks for subtitle service methods.

--- internal/application/service/subtitles/service.go
+++ internal/application/service/subtitles/service.go
@@ -1,8 +1,32 @@
 package subtitles
 
+import (
+	"github.com/PanovAlexey/learn-subtitles/internal/config"
+	"github.com/PanovAlexey/learn-subtitles/internal/domain/entity"
+)
+
 type SubtitlesService struct {
+	config config.Config
 }
 
-func NewSubtitlesService() SubtitlesService {
-	return SubtitlesService{}
+func NewSubtitlesService(config config.Config) SubtitlesService {
+	return SubtitlesService{
+		config: config,
+	}
+}
+
+func (s SubtitlesService) Add(userId int, subtitle string, forbiddenParts []string) error {
+	return nil
+}
+
+func (s SubtitlesService) GetList(userId int) ([]entity.Subtitle, error) {
+	return nil, nil
+}
+
+func (s SubtitlesService) GetById(userId, id int) (*entity.Subtitle, error) {
+	return nil, nil
+}
+
+func (s SubtitlesService) Delete(userId, id int) error {
+	return nil
 }

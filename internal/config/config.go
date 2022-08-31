233f934fcb2de689283c3b11fd0471086550609c
Panov Alexey <alexeykapanov@gmail.com>
2022-08-31 17:07:01 +0300

    Add telegram bot update timeout parameter to env and config.

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,15 @@ package config
 import (
 	"errors"
 	"os"
+	"strconv"
 )
 
 var configSingleton *Config
 
 type Config struct {
-	applicationName  string
-	telegramBotToken string
+	applicationName          string
+	telegramBotToken         string
+	telegramBotUpdateTimeout int
 }
 
 func NewConfig() (*Config, error) {
@@ -36,9 +38,18 @@ func (c Config) GetTelegramBotToken() string {
 	return c.telegramBotToken
 }
 
+func (c Config) GetTelegramBotUpdateTimeout() int {
+	return c.telegramBotUpdateTimeout
+}
+
 func (c *Config) initConfigByEnv() error {
 	c.applicationName = getEnv("APPLICATION_NAME")
 	c.telegramBotToken = getEnv("TELEGRAM_BOT_TOKEN")
+	telegramBotUpdateTimeout := getEnv("TELEGRAM_BOT_UPDATE_TIMEOUT")
+
+	if len(telegramBotUpdateTimeout) > 0 {
+		c.telegramBotUpdateTimeout, _ = strconv.Atoi(telegramBotUpdateTimeout)
+	}
 
 	if len(c.telegramBotToken) < 1 {
 		return errors.New("telegram bot token is empty")
@@ -58,5 +69,9 @@ func getEnv(key string) string {
 func (c *Config) initConfigByDefault() {
 	if len(c.applicationName) < 1 {
 		c.applicationName = "Learn subtitles"
+	}
+
+	if c.telegramBotUpdateTimeout == 0 {
+		c.telegramBotUpdateTimeout = 60
 	}
 }

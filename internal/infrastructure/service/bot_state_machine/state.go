e9df2f6a5bd542cf38e666c93d6281a682e29ec3
Panov Alexey <alexeykapanov@gmail.com>
2022-09-01 19:03:50 +0300

    Actualize state interface..

--- internal/infrastructure/service/bot_state_machine/state.go
+++ internal/infrastructure/service/bot_state_machine/state.go
@@ -3,10 +3,10 @@ package bot_state_machine
 import "github.com/PanovAlexey/learn-subtitles/internal/domain/entity"
 
 type State interface {
-	AddSubtitles() (*entity.Subtitle, error)
-	AddSubtitlesName(name string) (*entity.Subtitle, error)
-	AddSubtitlesText(text string) (*entity.Subtitle, error)
-	AddSubtitlesForbiddenParts(forbiddenParts []string) (*entity.Subtitle, error)
+	AddSubtitles() error
+	AddSubtitlesName(name string) error
+	AddSubtitlesText(text string) error
+	AddForbiddenPartsAndSaveSubtitles(subtitles entity.Subtitle, forbiddenPartsString string) error
 	GetSubtitlesList() ([]entity.Subtitle, error)
 	GetSubtitlesById() (*entity.Subtitle, error)
 	DeleteSubtitlesById() error

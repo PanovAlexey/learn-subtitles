ee98842ba26b7575b055b4eabe95d62e6940ce16
Panov Alexey <alexeykapanov@gmail.com>
2022-09-01 18:58:28 +0300

    Implement the function of splitting the text with the replacement of spoilers in the map.

--- internal/application/service/subtitles/service.go
+++ internal/application/service/subtitles/service.go
@@ -30,3 +30,41 @@ func (s SubtitlesService) GetById(userId, id int) (*entity.Subtitle, error) {
 func (s SubtitlesService) Delete(userId, id int) error {
 	return nil
 }
+
+
+func (s SubtitlesService) GetForbiddenPartsMapByString(data string) map[string]string {
+	forbiddenPartsMap := map[string]string{}
+	lines := strings.Split(data, "\n")
+
+	for _, line := range lines {
+		replacementArray := strings.Split(line, "=")
+
+		if len(replacementArray) < 1 || len(replacementArray) > 2 {
+			continue
+		} else if len(replacementArray) == 1 {
+			left := strings.TrimSpace(replacementArray[0])
+
+			if left == "" {
+				continue
+			}
+
+			forbiddenPartsMap[left] = " "
+		} else if len(replacementArray) == 2 {
+			left := strings.TrimSpace(replacementArray[0])
+
+			if left == "" {
+				continue
+			}
+
+			right := strings.TrimSpace(replacementArray[1])
+
+			if right == "" {
+				right = " "
+			}
+
+			forbiddenPartsMap[left] = right
+		}
+	}
+
+	return forbiddenPartsMap
+}

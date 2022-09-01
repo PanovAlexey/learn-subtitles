cf334258d171617d8ba59ae2d527a54b2a1c7ad0
Panov Alexey <alexeykapanov@gmail.com>
2022-09-01 19:01:26 +0300

    Add ForbiddenParts field to subtitles entity.

--- internal/domain/entity/subtitle.go
+++ internal/domain/entity/subtitle.go
@@ -3,9 +3,10 @@ package entity
 import "time"
 
 type Subtitle struct {
-	Name      string
-	Text      string
-	CreatedAt time.Time
-	Author    User
-	IsDeleted bool
+	Name           string
+	Text           string
+	CreatedAt      time.Time
+	Author         User
+	IsDeleted      bool
+	ForbiddenParts map[string]string
 }

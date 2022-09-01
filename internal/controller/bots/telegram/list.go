29c1dfb19527d9c088d2c0a865782fd236fecf13
Panov Alexey <alexeykapanov@gmail.com>
2022-09-01 19:00:19 +0300

    Create blank handler for getting subtitles list.

--- internal/controller/bots/telegram/list.go
+++ internal/controller/bots/telegram/list.go
@@ -5,14 +5,18 @@ import (
 	"strconv"
 )
 
-func (r CommandRouter) listCommand(inputMesage tgbotapi.Message) {
-	r.logger.Info("list handler. from: " + inputMesage.From.UserName)
+func (r CommandRouter) listCommand(inputMessage tgbotapi.Message) {
+	r.logger.Info("list handler. from: " + inputMessage.From.UserName)
+
+	dialog, _ := r.userStateService.GetUserDialog(strconv.FormatInt(inputMessage.Chat.ID, 10))
+
+	dialog.SetHasSubtitlesListState()
 
 	listMsg := "<strong>Pulp Fiction script</strong> \nType:<strong>subtitles</strong>\nGet: /download1001 \n\n" +
 		"<strong>Harry Potter and chamber of secrets</strong> \nType:<strong>book</strong>\nGet: /download1002 \n\n" //ToDo: change for real data
 	count := 2 //ToDo: change for real data
 
-	msg := tgbotapi.NewMessage(inputMesage.Chat.ID, "Your subtitles list (<strong>"+strconv.Itoa(count)+"</strong> pieces): \n\n"+listMsg)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Your subtitles list (<strong>"+strconv.Itoa(count)+"</strong> pieces): \n\n"+listMsg)
 	msg.ParseMode = tgbotapi.ModeHTML
 	r.bot.Send(msg)
 }

85815fb6d1213f7d043c7260a1cdabfc195f8ef7
Panov Alexey <alexeykapanov@gmail.com>
2022-09-01 19:03:21 +0300

    Actualize default handler.

--- internal/controller/bots/telegram/default.go
+++ internal/controller/bots/telegram/default.go
@@ -4,11 +4,29 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func (r CommandRouter) defaultBehavior(inputMesage tgbotapi.Message) {
-	r.logger.Info("default handler. from: " + inputMesage.From.UserName + ". text:" + inputMesage.Text)
+func (r CommandRouter) defaultBehavior(inputMessage tgbotapi.Message) {
+	r.logger.Info("default handler. from: " + inputMessage.From.UserName + ". text:" + inputMessage.Text)
+
+	dialog, _ := r.userStateService.GetUserDialog(strconv.FormatInt(inputMessage.Chat.ID, 10))
+
+	resultText := ""
+	info, err := dialog.TryToHandleUserData(inputMessage.Text)
+
+	if err != nil {
+		r.logger.Debug("tg bot default handler: ", err)
+		resultText = "You wrote: <i>" + inputMessage.Text + "</i>.\nInvalid input. Please repeat.\n\n" /* + r.getAvailableCommandListString()*/
+	} else {
+		resultText = info
+	}
 
 	msg := tgbotapi.NewMessage(inputMesage.Chat.ID, "You wrote: "+inputMesage.Text)
 	msg.ReplyToMessageID = inputMesage.MessageID
+	msg := tgbotapi.NewMessage(
+		inputMessage.Chat.ID,
+		resultText,
+	)
+	msg.ParseMode = tgbotapi.ModeHTML
+	msg.ReplyToMessageID = inputMessage.MessageID
 
 	r.bot.Send(msg)
 }

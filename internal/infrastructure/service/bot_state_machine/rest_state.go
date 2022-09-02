ad274fb510c665fead268dcaee451da6f9deae8a
Panov Alexey <alexeykapanov@gmail.com>
2022-09-02 06:22:00 +0300

    Add blank for RestState state.

---
+++ internal/infrastructure/service/bot_state_machine/rest_state.go
@@ -0,0 +1,60 @@
+package bot_state_machine
+
+import (
+	"errors"
+	"github.com/PanovAlexey/learn-subtitles/internal/domain/entity"
+)
+
+type RestState struct {
+	dialog *Dialog
+}
+
+func (s *RestState) GetCode() StatesEnum {
+	return Rest
+}
+
+func (s *RestState) AddSubtitles() error {
+	s.dialog.SetReadyToAddSubtitlesNameState()
+
+	return nil
+}
+
+func (s *RestState) AddSubtitlesName(name string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *RestState) AddSubtitlesText(text string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *RestState) AddForbiddenPartsAndSaveSubtitles(subtitles entity.Subtitle, forbiddenPartsString string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *RestState) GetSubtitlesList() ([]entity.Subtitle, error) {
+	return []entity.Subtitle{}, nil
+}
+
+func (s *RestState) GetSubtitlesById() (*entity.Subtitle, error) {
+	return &entity.Subtitle{}, nil
+}
+
+func (s *RestState) DeleteSubtitlesById() error {
+	return nil
+}
+
+func (s *RestState) GetRandomPhraseBySubtitlesId() (*entity.Phrase, error) {
+	return &entity.Phrase{}, nil
+}
+
+func (s *RestState) GetTranslateByPhraseId() (*entity.PhraseTranslation, error) {
+	return &entity.PhraseTranslation{}, nil
+}
+
+func (s *RestState) SetTranslateByPhraseId(translation string) error {
+	return nil
+}
+
+func (s *RestState) HideThisPhraseById() error {
+	return nil
+}

0f9db415b83a75b09a6b46e7ac96e806e2356a39
Panov Alexey <alexeykapanov@gmail.com>
2022-09-02 06:07:00 +0300

    Add blank for ReadyToAddTranslationRandomPhraseState state.

---
+++ internal/infrastructure/service/bot_state_machine/ready_to_add_translation_random_phrase_state.go
@@ -0,0 +1,60 @@
+package bot_state_machine
+
+import (
+	"errors"
+	"github.com/PanovAlexey/learn-subtitles/internal/domain/entity"
+)
+
+type ReadyToAddTranslationRandomPhraseState struct {
+	dialog *Dialog
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) GetCode() StatesEnum {
+	return ReadyToAddTranslationRandomPhrase
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) AddSubtitles() error {
+	s.dialog.SetReadyToAddSubtitlesNameState()
+
+	return nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) AddSubtitlesName(name string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) AddSubtitlesText(text string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) AddForbiddenPartsAndSaveSubtitles(subtitles entity.Subtitle, forbiddenPartsString string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) GetSubtitlesList() ([]entity.Subtitle, error) {
+	return []entity.Subtitle{}, nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) GetSubtitlesById() (*entity.Subtitle, error) {
+	return &entity.Subtitle{}, nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) DeleteSubtitlesById() error {
+	return nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) GetRandomPhraseBySubtitlesId() (*entity.Phrase, error) {
+	return &entity.Phrase{}, nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) GetTranslateByPhraseId() (*entity.PhraseTranslation, error) {
+	return &entity.PhraseTranslation{}, nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) SetTranslateByPhraseId(translation string) error {
+	return nil
+}
+
+func (s *ReadyToAddTranslationRandomPhraseState) HideThisPhraseById() error {
+	return nil
+}

3e9f73f8630f69f2eab0b451ff3b0fb3cecbdcdd
Panov Alexey <alexeykapanov@gmail.com>
2022-09-02 06:43:00 +0300

    Add blank for HasSubtitlesListState state.

---
+++ internal/infrastructure/service/bot_state_machine/has_subtitles_list_state.go
@@ -0,0 +1,60 @@
+package bot_state_machine
+
+import (
+	"errors"
+	"github.com/PanovAlexey/learn-subtitles/internal/domain/entity"
+)
+
+type HasSubtitlesListState struct {
+	dialog *Dialog
+}
+
+func (s *HasSubtitlesListState) GetCode() StatesEnum {
+	return HasSubtitlesList
+}
+
+func (s *HasSubtitlesListState) AddSubtitles() error {
+	s.dialog.SetReadyToAddSubtitlesNameState()
+
+	return nil
+}
+
+func (s *HasSubtitlesListState) AddSubtitlesName(name string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *HasSubtitlesListState) AddSubtitlesText(text string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *HasSubtitlesListState) AddForbiddenPartsAndSaveSubtitles(subtitles entity.Subtitle, forbiddenPartsString string) error {
+	return errors.New("not available command for current state")
+}
+
+func (s *HasSubtitlesListState) GetSubtitlesList() ([]entity.Subtitle, error) {
+	return []entity.Subtitle{}, nil
+}
+
+func (s *HasSubtitlesListState) GetSubtitlesById() (*entity.Subtitle, error) {
+	return &entity.Subtitle{}, nil
+}
+
+func (s *HasSubtitlesListState) DeleteSubtitlesById() error {
+	return nil
+}
+
+func (s *HasSubtitlesListState) GetRandomPhraseBySubtitlesId() (*entity.Phrase, error) {
+	return &entity.Phrase{}, nil
+}
+
+func (s *HasSubtitlesListState) GetTranslateByPhraseId() (*entity.PhraseTranslation, error) {
+	return &entity.PhraseTranslation{}, nil
+}
+
+func (s *HasSubtitlesListState) SetTranslateByPhraseId(translation string) error {
+	return nil
+}
+
+func (s *HasSubtitlesListState) HideThisPhraseById() error {
+	return nil
+}

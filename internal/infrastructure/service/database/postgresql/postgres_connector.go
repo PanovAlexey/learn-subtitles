797c2ab9e7b3c314140b260472ce96cd672d0ee4
Panov Alexey <alexeykapanov@gmail.com>
2022-09-09 16:28:59 +0300

    Create conntector for postgresql.

---
+++ internal/infrastructure/service/database/postgresql/postgres_connector.go
@@ -0,0 +1,38 @@
+package postgresql
+
+import (
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+const TableUsersName = `users`
+
+func GetPostgresConnector(
+	databaseUser, databasePassword, databaseAddress, databasePort, databaseName string,
+	maxOpenConnections, maxIdleConnections int,
+	connectionMaxIdleTime, connectionMaxLifeTime time.Duration,
+) (*sqlx.DB, error) {
+	db, err := sqlx.Open(
+		"pgx",
+		"postgresql://"+databaseUser+":"+databasePassword+"@"+databaseAddress+
+			":"+databasePort+"/"+databaseName+"?sslmode=disable",
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConnections)
+	db.SetMaxIdleConns(maxIdleConnections)
+	db.SetConnMaxIdleTime(connectionMaxIdleTime)
+	db.SetConnMaxLifetime(connectionMaxLifeTime)
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}

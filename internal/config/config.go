89f0a925a19c53e6264fd5ba4feb1aaa4a3e4085
Panov Alexey <alexeykapanov@gmail.com>
2022-09-09 16:28:22 +0300

    Add databases environmets to config\.

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"strconv"
+	"time"
 )
 
 var configSingleton *Config
@@ -12,6 +13,16 @@ type Config struct {
 	applicationName          string
 	telegramBotToken         string
 	telegramBotUpdateTimeout int
+
+	databaseUser          string
+	databasePassword      string
+	databaseAddress       string
+	databasePort          string
+	databaseName          string
+	maxOpenConnections    int
+	maxIdleConnections    int
+	connectionMaxIdleTime time.Duration
+	connectionMaxLifeTime time.Duration
 }
 
 func NewConfig() (*Config, error) {
@@ -42,6 +53,42 @@ func (c Config) GetTelegramBotUpdateTimeout() int {
 	return c.telegramBotUpdateTimeout
 }
 
+func (c Config) GetDatabaseUser() string {
+	return c.databaseUser
+}
+
+func (c Config) GetDatabasePassword() string {
+	return c.databasePassword
+}
+
+func (c Config) GetDatabaseAddress() string {
+	return c.databaseAddress
+}
+
+func (c Config) GetDatabasePort() string {
+	return c.databasePort
+}
+
+func (c Config) GetDatabaseName() string {
+	return c.databaseName
+}
+
+func (c Config) GetMaxOpenConnections() int {
+	return c.maxOpenConnections
+}
+
+func (c Config) GetMaxIdleConnections() int {
+	return c.maxIdleConnections
+}
+
+func (c Config) GetConnectionMaxIdleTime() time.Duration {
+	return c.connectionMaxIdleTime
+}
+
+func (c Config) GetConnectionMaxLifeTime() time.Duration {
+	return c.connectionMaxLifeTime
+}
+
 func (c *Config) initConfigByEnv() error {
 	c.applicationName = getEnv("APPLICATION_NAME")
 	c.telegramBotToken = getEnv("TELEGRAM_BOT_TOKEN")
@@ -53,6 +100,48 @@ func (c *Config) initConfigByEnv() error {
 
 	if len(c.telegramBotToken) < 1 {
 		return errors.New("telegram bot token is empty")
+	}
+
+	c.databaseUser = getEnv("DB_USER")
+	c.databasePassword = getEnv("DB_PASSWORD")
+	c.databaseAddress = getEnv("DB_ADDRESS")
+	c.databasePort = getEnv("DB_PORT")
+	c.databaseName = getEnv("DB_NAME")
+
+	maxOpenConnections := getEnv("DB_MAX_OPEN_CONNECTIONS")
+
+	if len(maxOpenConnections) > 0 {
+		c.maxOpenConnections, _ = strconv.Atoi(maxOpenConnections)
+	}
+
+	maxIdleConnections := getEnv("DB_MAX_IDLE_CONNECTIONS")
+
+	if len(maxIdleConnections) > 0 {
+		c.maxIdleConnections, _ = strconv.Atoi(maxIdleConnections)
+	}
+
+	connectionMaxIdleTime := getEnv("DB_CONNECTION_MAX_IDLE_TIME")
+
+	if len(connectionMaxIdleTime) > 0 {
+		connectionMaxIdleTimeInt64, err := strconv.ParseInt(connectionMaxIdleTime, 10, 64)
+
+		if err != nil {
+			return err
+		}
+
+		c.connectionMaxLifeTime = time.Duration(connectionMaxIdleTimeInt64)
+	}
+
+	connectionMaxLifeTime := getEnv("DB_CONNECTION_MAX_LIFE_TIME")
+
+	if len(connectionMaxLifeTime) > 0 {
+		connectionMaxLifeTimeInt64, err := strconv.ParseInt(connectionMaxLifeTime, 10, 64)
+
+		if err != nil {
+			return err
+		}
+
+		c.connectionMaxLifeTime = time.Duration(connectionMaxLifeTimeInt64)
 	}
 
 	return nil

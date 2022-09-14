3d92480a50308411a6d90c00b875b72df0a99a05
Panov Alexey <alexeykapanov@gmail.com>
2022-09-14 17:08:35 +0300

    Create service for user entity.

---
+++ internal/application/service/user/serivce.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"github.com/PanovAlexey/learn-subtitles/internal/domain/dto"
+	"github.com/PanovAlexey/learn-subtitles/internal/domain/entity"
+)
+
+type UserRepository interface {
+	SaveUser(user entity.User) (dto.UserDatabaseDto, error)
+	GetUserByLogin(login string) (dto.UserDatabaseDto, error)
+}
+
+type UserService struct {
+	userRepository UserRepository
+}
+
+func NewUserService(userRepository UserRepository) UserService {
+	return UserService{userRepository: userRepository}
+}
+
+func (s UserService) SaveUser(user entity.User) (dto.UserDatabaseDto, error) {
+	return s.userRepository.SaveUser(user)
+}
+
+func (s UserService) GetUserByLogin(login string) (dto.UserDatabaseDto, error) {
+	user, err := s.userRepository.GetUserByLogin(login)
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, err
+}
